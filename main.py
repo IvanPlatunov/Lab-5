01f7da813c20450f7e73d272cf8a375fbf4bb33a
ivashka <ivankubik2007@gmail.com>
2025-11-05 15:51:14 +0300

    Additional task solution

--- main.py
+++ main.py
@@ -40,3 +40,19 @@ for i in range(len(arr_id)):
     table.append((arr_id[i], arr_sur[i], arr_em[i], arr_date[i], arr_site[i]))
 df = pd.DataFrame(table, columns=header)
 df.to_csv('result.csv', index=False)
+
+#Additional task
+f5 = open('task_add.txt', errors='ignore').read()
+email = r'[a-zA-Z0-9]+\@[a-zA-Z0-9-]+\.[a-z]{2,}'
+date = r'(\d+\-\d+\-\d+)|(\d+\/\d+\/\d+)|(\d+\.\d+\.\d+)'
+site = r'(?:http|https)://(?:www\.|)[a-zA-Z-]+\.[a-z]+'
+match_e = re.findall(email, f5)
+match_d = re.findall(date, f5)
+for i in range(len(match_d)):
+    for j in match_d[i]:
+        if j != '':
+            match_d[i] = j
+match_s = re.findall(site, f5)
+print('Email', match_e)
+print('Date', match_d)
+print('Site', match_s)
